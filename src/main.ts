1bde0b91fcb8105aa64bb9efd8e8f5df80004528
antoniomagfig <antoniomagalhaes.20241@poli.ufrj.br>
2026-01-02 11:45:30 -0300

    main

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,27 @@
-import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+  import { ValidationPipe } from '@nestjs/common';
+  import { NestFactory } from '@nestjs/core';
+  import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-    }),
-  );
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
 
-  app.enableCors({
-    origin: 
-      '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
-  });
+    app.enableCors({
+      origin: 
+        '*',
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      credentials: true,
+    });
 
-  const port = process.env.PORT || 3100;
-  await app.listen(port);
-}
-bootstrap();
+    const port = Number(process.env.PORT);
+    await app.listen(port);
+    console.log(`Listening on port ${port}`);
+  }
+  bootstrap();
